f0ffa0ca583ff08d1e38d488723f992d40938c1e
swl16 <sweili165@gmail.com>
2026-03-09 22:44:35 +0800

    changed

--- Cinema/src/cinema/Cinema.java
+++ Cinema/src/cinema/Cinema.java
@@ -1,21 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package cinema;
 
-/**
- *
- * @author WEI LI
- */
+import javax.swing.*;
+
 public class Cinema {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-        new LoginPage();
+        SwingUtilities.invokeLater(new Runnable(){
+            @Override
+            public void run(){
+                new LoginPage();
+            }
+        }); 
     }
     
 }
